c18a3823eecd4869a2cbcea9caefb0f07c5deb00
Kenta Murata <mrkn@cookpad.com>
2014-03-03 19:22:58 +0900

    Refactored

--- chap10/10_1_rpn/rpn.hs
+++ chap10/10_1_rpn/rpn.hs
@@ -5,16 +5,18 @@ evalRPNTokens :: [String] -> Double
 evalRPNTokens = head . (foldl evalRPNToken [])
 
 evalRPNToken :: [Double] -> String -> [Double]
-evalRPNToken (x1:x2:xs) "+" = (x2 + x1):xs
-evalRPNToken (x1:x2:xs) "-" = (x2 - x1):xs
-evalRPNToken (x1:x2:xs) "*" = (x2 * x1):xs
-evalRPNToken (x1:x2:xs) "/" = (x2 / x1):xs
-evalRPNToken (x1:x2:xs) "**" = (x2 ** x1):xs
-evalRPNToken (x:xs) "exp" = (exp x):xs
-evalRPNToken (x:xs) "log" = (log x):xs
-evalRPNToken (x:xs) "sin" = (sin x):xs
-evalRPNToken (x:xs) "cos" = (cos x):xs
-evalRPNToken (x:xs) "tan" = (tan x):xs
-evalRPNToken xs "sum" = [sum xs]
-evalRPNToken xs "mean" = [(sum xs) / (fromIntegral $ length xs)]
-evalRPNToken xs s = (read s):xs
+evalRPNToken = f
+  where
+    f (x1:x2:xs) "+" = (x2 + x1):xs
+    f (x1:x2:xs) "-" = (x2 - x1):xs
+    f (x1:x2:xs) "*" = (x2 * x1):xs
+    f (x1:x2:xs) "/" = (x2 / x1):xs
+    f (x1:x2:xs) "**" = (x2 ** x1):xs
+    f (x:xs) "exp" = (exp x):xs
+    f (x:xs) "log" = (log x):xs
+    f (x:xs) "sin" = (sin x):xs
+    f (x:xs) "cos" = (cos x):xs
+    f (x:xs) "tan" = (tan x):xs
+    f xs "sum" = [sum xs]
+    f xs "mean" = [(sum xs) / (fromIntegral $ length xs)]
+    f xs s = (read s):xs
